3c29f403828933415752cf0313b795f2fb4d6530
gjz <guijingzhaowork@gmail.com>
2025-03-05 23:02:05 +0800

    05 write unit test for crud operation

--- db/sqlc/models.go
+++ db/sqlc/models.go
@@ -9,7 +9,7 @@ import (
 	"time"
 )
 
-type Accounts struct {
+type Account struct {
 	ID        int64     `json:"id"`
 	Owner     string    `json:"owner"`
 	Balance   int64     `json:"balance"`
@@ -17,7 +17,7 @@ type Accounts struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-type Entries struct {
+type Entry struct {
 	ID        int64         `json:"id"`
 	AccountID sql.NullInt64 `json:"account_id"`
 	// could be negtive or positive
@@ -25,7 +25,7 @@ type Entries struct {
 	CreatedAt time.Time `json:"created_at"`
 }
 
-type Transfers struct {
+type Transfer struct {
 	ID            int64 `json:"id"`
 	FromAccountID int64 `json:"from_account_id"`
 	ToAccountID   int64 `json:"to_account_id"`
